04cdf95c04f711868425e42b8c9ccc38e2b1ba98
qsona <mori.jmk@gmail.com>
2019-05-14 23:22:51 +0900

    ittan

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,149 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+import { useState, useEffect } from 'react';
+import { Client } from 'boardgame.io/react';
+import { IGameCtx } from 'boardgame.io/core';
+import { ShizuokaPokerGame, GameState, ShizuokaPokerAI, ICard } from './ShizuokaPoker'
+
+interface IProps {
+  moves: any;
+  events: any;
+  isActive: boolean;
+  G: GameState;
+  ctx: IGameCtx;
+}
+
+const ShizuokaPokerBoard: React.FC<IProps> = (props) => {
+  const [selectedBoardCard, setSelectedBoardCard] = useState<ICard | null>(null);
+  const [selectedMyCard, setSelectedMyCard] = useState<ICard | null>(null);
+  const [isTrashSelected, setTrashSelected] = useState<boolean>(false);
+
+
+  function toggleMyCard(card: ICard) {
+    setSelectedMyCard(card === selectedMyCard ? null : card);
+  }
+
+  function toggleBoardCard(card: ICard) {
+    console.log(card)
+    console.log(card === selectedBoardCard)
+    setSelectedBoardCard(card === selectedBoardCard ? null : card);
+    setTrashSelected(false);
+  }
+
+  function toggleTrash() {
+    setTrashSelected(!isTrashSelected);
+    setSelectedBoardCard(null);
+  }
+
+  // function checkSelectedCards() {
+  useEffect(() => {
+    console.log(`${selectedBoardCard} ${selectedMyCard} ${isTrashSelected}`)
+    if (selectedMyCard) {
+      if (isTrashSelected) {
+        // TODO: move!
+        console.log("change to trash")
+        props.moves.throwAndChange(selectedMyCard)
+        resetSelectedCards()
+      } else if (selectedBoardCard) {
+        // TODO: move!
+        console.log("change to card")
+        props.moves.change(selectedMyCard, selectedBoardCard)
+        resetSelectedCards()
+      }
+    }
+  }, [selectedBoardCard, selectedMyCard, isTrashSelected]);
+
+  function resetSelectedCards() {
+    setSelectedBoardCard(null);
+    setSelectedMyCard(null);
+    setTrashSelected(false);
+  }
+
+  const cellStyle: React.CSSProperties = {
+    border: '1px solid #555',
+    width: '50px',
+    height: '50px',
+    lineHeight: '50px',
+    textAlign: 'center',
+  };
+
+  const tboard = props.G.board.map((c, i) => {
+    return (
+      <td style={cellStyle} key={i} onClick={() => toggleBoardCard(c)}>
+        {c}
+      </td>
+    )
+  });
+  const thand = props.G.players[0].hand.map((c, i) => {
+    const style: React.CSSProperties = { ...cellStyle };
+    if (props.G.publicHands[0].includes(c)) {
+      style.backgroundColor = 'gray'
+    }
+    return (
+      <td style={style} key={i} onClick={() => toggleMyCard(c)}>
+        {c}
+      </td>
+    )
+  })
+  const topponent = [0, 1, 2, 3, 4].map(i => {
+    const c = props.G.publicHands[1][i] || '';
+    return (
+      <td style={cellStyle} key={i} >
+        {c}
+      </td>
+    )
+  })
+
+  // let tbody: JSX.Element[] = [];
+  // for (let i = 0; i < 3; i++) {
+  //   let cells: JSX.Element[] = [];
+  //   for (let j = 0; j < 3; j++) {
+  //     const id = 3 * i + j;
+  //     cells.push(
+  //       <td style={cellStyle} key={id} onClick={() => onClick(id)}>
+  //         {props.G.cells[id]}
+  //       </td>
+  //     );
+  //   }
+  //   tbody.push(<tr key={i}>{cells}</tr>);
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>opponent public hand</p>
+      <table id="opponent"><tbody><tr>{topponent}</tr></tbody></table>
+      <p>board</p>
+      <table id="board"><tbody><tr>{tboard}</tr></tbody></table>
+      <p>hand</p>
+      <table id="hand"><tbody><tr>{thand}</tr></tbody></table>
+      <table id="trash"><tbody><tr>
+        <td style={cellStyle} key="0" onClick={() => toggleTrash()}>Trash</td>
+      </tr></tbody></table>
+
+      <p>---logs---</p>
+
+      <p>board: {JSON.stringify(props.G.board)}</p>
+      <p>hand: {JSON.stringify(props.G.players[0].hand)}</p>
+      <p>publicHand 0: {JSON.stringify(props.G.publicHands[0])}</p>
+      <p>publicHand 1: {JSON.stringify(props.G.publicHands[1])}</p>
+      <p>trash: {JSON.stringify(props.G.trash)}</p>
     </div>
   );
 }
 
-export default App;
+const App: any = Client({
+  game: ShizuokaPokerGame,
+  board: ShizuokaPokerBoard,
+  ai: ShizuokaPokerAI,
+});
+
+const RealApp: React.FC = () => {
+  return (
+    <div>
+      <App gameID="gameid" playerID="0"></App>
+    </div>
+  )
+}
+
+export default RealApp;
---
+++ src/ShizuokaPoker.ts
@@ -0,0 +1,143 @@
+import _ from 'lodash'
+import { Game, PlayerView, IGameCtx } from 'boardgame.io/core';
+import { AI, IAIMoveObj } from 'boardgame.io/ai'
+import assert from 'power-assert';
+
+export type ICard = string;
+
+type ISecretState = { deck: ICard[] };
+type IPlayerState = { hand: ICard[] };
+
+export type GameState = {
+  secret: ISecretState,
+  board: ICard[],
+  trash: ICard[],
+  publicHands: { '0': ICard[], '1': ICard[] },
+  players: { '0': IPlayerState, '1': IPlayerState },
+  stopped: boolean,
+}
+
+// card helpers
+
+const CARD_NUMS: string[] = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'];
+const CARD_SUITS: string[] = ['s', 'd', 'h', 'c'];
+const makeCard = (cardNum: string, cardSuit: string): ICard => `${cardNum}${cardSuit}`
+
+const moveCard = (src: ICard[], card: ICard, dest: ICard[]) => {
+  const i = src.indexOf(card);
+  assert(i !== -1);
+  _.pullAt(src, i);
+  dest.push(card);
+}
+
+const change = (G: GameState, ctx: IGameCtx, myHandCard: ICard, boardCard: ICard) => {
+  const secretHand = G.players[ctx.currentPlayer].hand;
+  const publicHand = G.publicHands[ctx.currentPlayer];
+  const srcHand = secretHand.includes(myHandCard) ? secretHand :
+    publicHand.includes(myHandCard) ? publicHand : assert(false) as never;
+
+  const board = G.board;
+  assert(board.includes(boardCard));
+
+  moveCard(srcHand, myHandCard, board);
+  moveCard(board, boardCard, publicHand);
+}
+
+const throwAndChange = (G: GameState, ctx: IGameCtx, myHandCard: ICard, boardCard: ICard) => {
+  const secretHand = G.players[ctx.currentPlayer].hand;
+  const publicHand = G.publicHands[ctx.currentPlayer];
+  const srcHand = secretHand.includes(myHandCard) ? secretHand :
+    publicHand.includes(myHandCard) ? publicHand : assert(false) as never;
+
+  const board = G.board;
+  assert(board.includes(boardCard));
+
+  const deck = G.secret.deck;
+
+  moveCard(srcHand, myHandCard, board);
+  moveCard(board, boardCard, G.trash);
+  moveCard(deck, deck[0], secretHand);
+}
+
+export const ShizuokaPokerGame = Game<GameState>({
+  name: 'shizuoka-poker',
+  setup: () => {
+    const allCards: ICard[] = [];
+    CARD_NUMS.forEach(n => {
+      CARD_SUITS.forEach(s => {
+        allCards.push(makeCard(n, s));
+      });
+    });
+    allCards.push(CARD_NUMS[0])
+    const deck = _.shuffle(allCards);
+    const board: ICard[] = [];
+    const p0Hand: ICard[] = [];
+    const p1Hand: ICard[] = [];
+    _.times(5, () => board.push(deck.pop()!));
+    _.times(5, () => p0Hand.push(deck.pop()!));
+    _.times(5, () => p1Hand.push(deck.pop()!));
+
+    return {
+      secret: {
+        deck,
+      },
+      board,
+      trash: [],
+      publicHands: {
+        '0': [],
+        '1': [],
+      },
+      players: {
+        '0': {
+          hand: p0Hand,
+        },
+        '1': {
+          hand: p1Hand,
+        },
+      },
+      stopped: false,
+    };
+  },
+  moves: {
+    change,
+    throwAndChange,
+    skip: (G) => G,
+    stop: (G) => G.stopped = true,
+  },
+  flow: {
+    movesPerTurn: 1,
+    phases: [
+      {
+        name: 'change',
+        allowedMoves: ['change', 'throwAndChange', 'skip'],
+        endPhaseIf: (G, ctx) => ctx.turn === 6,
+      },
+      {
+        name: 'stoppableChange',
+        allowedMoves: ['change', 'throwAndChange', 'skip', 'stop'],
+        endPhaseIf: (G) => G.stopped,
+      },
+      {
+        name: 'lastChange',
+        allowedMoves: ['change', 'throwAndChange', 'skip'],
+        endPhaseIf: () => true, // TODO
+      },
+    ],
+    endGameIf: (G, ctx) => {
+    }
+  },
+  playerView: PlayerView.STRIP_SECRETS
+})
+
+export const ShizuokaPokerAI = AI<GameState>({
+  enumerate: (G, ctx) => {
+    let moves: IAIMoveObj[] = [
+      {
+        move: 'skip',
+        args: [],
+      }
+    ];
+    // TODO
+    return moves;
+  },
+})
